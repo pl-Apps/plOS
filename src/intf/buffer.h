d129f81539742ca5cbb2278d934cb0a7f9f1d96f
Peppo <pietrofacchini46@gmail.com>
2021-05-09 16:18:20 +0200

    New commit

--- src/intf/buffer.h
+++ src/intf/buffer.h
@@ -1,9 +1,8 @@
-/** @author Levente Kurusa <levex@linux.com> **/
 #ifndef __BUFFER_H_
 #define __BUFFER_H_
 
 #include <stdint.h>
 
-//extern void bprintf(uint32_t* buffer, char* fmt, ...);
+extern void bprintf(uint32_t* buffer, char* fmt, ...);
 
 #endif
--- src/intf/devfs.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DEVFS_H_
-#define __DEVFS_H_
-
-extern void devfs_init();
-extern uint8_t devfs_probe(device_t *dev);
-extern uint8_t devfs_mount(device_t *dev, void *priv);
-
-#endif
