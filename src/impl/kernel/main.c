66e83e803265bcb04904c769aead0f9a484fe2c3
Peppo <pietrofacchini46@gmail.com>
2021-05-09 10:18:59 +0200

    New commit

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,6 +2,6 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("plOS");
 }
