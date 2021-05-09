9eb926ff43a601b010d1c7e9dbb352380e95afe9
Peppo <pietrofacchini46@gmail.com>
2021-05-09 14:37:08 +0200

    New commit

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,19 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
-    print_str("plOS");
+    print_str("plOS\n\n");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("1) PL-CLI\n");
+    print_str("2) Video Settings\n");
+    print_str("3) Shutdown Options\n");
+}
+
+void videoSettings()
+{
+    
+}
+
+void ShutdownOptions()
+{
+
 }
