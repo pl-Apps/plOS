8a0e9e47d12368f6e1d377af9603d89645527f9b
Peppo <75394676+Peppooo@users.noreply.github.com>
2021-07-31 14:54:36 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-ubuntu1804 run "g++ -static sys/sys.cpp -o build/init; mkdir build; mkdir rootfs; cd build; find . | cpio -o -H newc | gzip > ../rootfs/rootfs.cpio.gz;"
-ubuntu1804 run "cd kernel; make isoimage FDINITRD=$(pwd)/../rootfs/rootfs.cpio.gz; rm -rf ../rootfs; rm -rf ../build;"
