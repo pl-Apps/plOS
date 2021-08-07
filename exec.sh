b95e1a8e93feaaccc4e177068af2117f569ade3a
Peppo <75394676+Peppooo@users.noreply.github.com>
2021-08-07 20:02:22 +0200

    Fixed linux execute script "file not found"

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
 bash -c "g++ -static sys/sys.cpp -liw -o build/init; mkdir build; mkdir rootfs; cd build; find . | cpio -o -H newc | gzip > ../rootfs/rootfs.cpio.gz"
-qemu-system-x86_64 -kernel kernel\arch\x86_64\boot\bzImage -initrd rootfs/rootfs.cpio.gz
+qemu-system-x86_64 -kernel kernel/arch/x86_64/boot/bzImage -initrd rootfs/rootfs.cpio.gz
